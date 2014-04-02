d0c877dd679d4d753b34a99250011c4f30e200e1
Seleznev Alexander <absenteg@gmail.com>
2014-04-02 21:54:34 +0400

    version 0.0.1

---
+++ README.md
@@ -0,0 +1,42 @@
+Copyright
+=======
+
+## ABOUT
+The Copyright plugin allows you to add a reference to the source in buffer when copying.
+
+### Browser Support 
+Plugin is supported in Internet Explorer 7+, Firefox 3.5+, Opera 10.5+, Chrome 4.0+, and Safari 4.0+.
+
+## USAGE
+The Copyright plugin uses the jQuery JavaScript library, only. So, include just these two javascript files in your header.
+
+<pre>
+&lt;script src="js/jquery.js"&gt;&lt;/script&gt;
+&lt;script src="js/jquery.coopyright.js">&lt;/script&gt;
+</pre>
+
+Call the jQuery Copyright plugin. You can select a specific selector or the document.
+
+<pre>$(document).copyright();</pre>
+
+### Options:
+You can pass an options object in plugin init method.
+* `text` : The text that will be added when copying (Default: `" Reference: " + window.location.href`);
+* `minlength` : The minimum length of the copied text when running the plugin (Default: `0`).
+
+<pre>$(document).copyright({ text: "&lt;br&gt;Reference: " + window.location.href, minlength: 100 });</pre>
+
+### Methods:
+You can call some methods. Just pass their name.
+* `remove` : Stop the plugin.
+
+<pre>$(document).copyright('remove');</pre>
+
+### Events: 
+You can listen Copyright events. 
+* `copyright_BeforeCopy`
+* `copyright_AfterCopy`
+
+
+## EXAMPLE
+[View example](http://htmlpreview.github.io/?https://github.com/absentik/copyright/blob/master/index.html#example)
---
+++ jquery.coopyright.js
@@ -0,0 +1,144 @@
+/*
+ * jQuery Copyright plugin 0.0.1 
+ * https://github.com/absentik/copyright
+ * 
+ * Author: Seleznev Alexander (ABSENT) 
+ * Email: absenteg@gmail.com 
+ * Website: http://whoisabsent.ru 
+ *  
+ * Licensed under the MIT license. 
+ * http://www.opensource.org/licenses/mit-license.php 
+ */
+
+;(function($, window, document, undefined){
+
+    var pluginName = "copyright";
+    var defaults = {
+        text: "<br>Reference: " + window.location.href, 
+        minlength: 0
+    };
+
+    function Copyright(element, options) {
+        this.element = element;
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.options = $.extend({}, defaults, options);
+        this.action = typeof options === "string" ? options : "default";
+        this.init();
+    }
+
+    Copyright.prototype.getSelectedText = function() {
+        var text = "";
+        if (window.getSelection) {
+            text = window.getSelection().toString();
+        } else if (document.selection) {
+            text = document.selection.createRange().text;
+        }
+        return text;
+    };
+
+    Copyright.prototype.setSelectionRange = function() {
+        if (window.getSelection) {
+            var range = document.createRange();
+            range.selectNodeContents(this.div);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (document.selection && document.body.createTextRange) {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(this.div);
+            textRange.select();
+        }
+    };
+
+    Copyright.prototype.removeSelectionRange = function() {
+        if (window.getSelection) {
+            window.getSelection().removeAllRanges();
+        } else if (document.selection) {
+            document.selection.empty();
+        }
+    };
+
+    Copyright.prototype.saveSelectionRange = function() {
+        if (window.getSelection) {
+            sel = window.getSelection();
+            if (sel.getRangeAt && sel.rangeCount) {
+                return sel.getRangeAt(0);
+            }
+        } else if (document.selection) {
+            return document.selection.createRange();
+        }
+        return null;
+    };
+
+    Copyright.prototype.restoreSelectionRange = function() {
+        if (this.range) {
+            if (window.getSelection) {
+                sel = window.getSelection();
+                sel.removeAllRanges();
+                sel.addRange(this.range);
+            } else if (document.selection) {
+                this.range.select();
+            }
+        }
+        else {
+            this.it.removeSelectionRange();
+        }
+    };
+
+    Copyright.prototype.copy = function() {
+        var it = this;
+        var body = $("body");
+        var selectText = it.getSelectedText();
+        var range = it.saveSelectionRange(body[0]);
+
+        if (selectText.length > it.options.minlength) {
+            selectText += it.options.text;
+        }
+
+        var div = $("<div/>", { 
+            html: selectText, 
+            style: "width: 1px; height: 1px; overflow: hidden; position: absolute; top: -99999px; left: -99999px; opacity: 0.01" 
+        }).appendTo(body);
+        it.setSelectionRange.call({ div: div[0] });
+
+        //after copy function
+        window.setTimeout(function() {
+            it.restoreSelectionRange.call({ it: it, range: range });
+            div.remove();
+            return $(it.element).trigger(pluginName + "_AfterCopy");
+        }, 0);
+
+        return $(it.element).trigger(pluginName + "_BeforeCopy");
+    };
+
+    Copyright.prototype.remove = function() {
+        var it = this;
+        $(it.element).off("copy");
+    };
+
+    Copyright.prototype.start = function() {
+        var it = this;
+        $(it.element).on("copy", function() { 
+            it.copy(); 
+        });
+    };
+
+    Copyright.prototype.init = function() {
+        switch (this.action) {
+            case "remove":
+                return this.remove();
+            break;
+            default:
+                return this.start();
+            break;
+        }
+    };
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            $.data(this, 'plugin_' + pluginName, new Copyright(this, options));
+        });
+    }
+
+})(jQuery, window, document);
